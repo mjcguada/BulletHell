4b4e648e19bda66eea0c356b7714f8dbcc997ad7
Mario Jimenez <mjcguada@gmail.com>
2023-03-26 18:28:13 +0200

    Scriptable objects for events created

---
+++ Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/GameEvent")]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise() 
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listenerToAdd) 
+    {
+        if (!listeners.Contains(listenerToAdd)) 
+        {
+            listeners.Add(listenerToAdd);
+        }
+    }
+
+    public void UnregisterListener(GameEventListener listenerToRemove) 
+    {
+        if (listeners.Contains(listenerToRemove)) 
+        {
+            listeners.Remove(listenerToRemove);
+        }
+    }
+}
---
+++ Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField] private GameEvent Event;
+    [SerializeField] private UnityEvent Response;
+
+    private void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventRaised() 
+    {
+        if (Response != null) 
+        {
+            Response.Invoke();
+        }
+    }
+}
